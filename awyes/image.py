fb20a67747e105b2210334ce075a4053ecd6c205
trumanpurnell <truman.purnell@gmail.com>
2021-05-09 22:28:58 -0700

    adds modifies build output to increase readability

--- awyes/image.py
+++ awyes/image.py
@@ -4,8 +4,9 @@ from pprint import pprint
 def deploy_images(self):
     for dockerfile in self.images:
         pprint([
-            line for line in
+            line['stream'] for line in
             self.__class__.docker_client.build(
-                fileobj=open(self.root + '/' + dockerfile, 'rb')
+                fileobj=open(self.root + '/' + dockerfile, 'rb'),
+                decode=True
             )
         ])
