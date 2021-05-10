bd1b1b95d26ea52de064c6c857a5a7f9d6a52200
trumanpurnell <truman.purnell@gmail.com>
2021-05-09 22:31:27 -0700

    cant win em all

--- awyes/image.py
+++ awyes/image.py
@@ -4,7 +4,7 @@ from pprint import pprint
 def deploy_images(self):
     for dockerfile in self.images:
         pprint([
-            line['stream'] for line in
+            line for line in
             self.__class__.docker_client.build(
                 fileobj=open(self.root + '/' + dockerfile, 'rb'),
                 decode=True
