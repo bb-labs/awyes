4c385ba4d3b37e539893a2f2e7210a666acc20bc
trumanpurnell <truman.purnell@gmail.com>
2021-05-12 13:27:39 -0700

    prints images after building

--- awyes/image.py
+++ awyes/image.py
@@ -4,15 +4,14 @@ from os.path import normpath, join
 
 def deploy_images(self):
     for dockerfile in self.images:
-        pprint([
-            line for line in
-            self.__class__.docker_client.build(
-                decode=True,
-                path=self.root_path,
-                dockerfile=normpath(join(
-                    self.root_path,
-                    self.config_path,
-                    dockerfile
-                )),
-            )
-        ])
+        self.__class__.docker_client.build(
+            decode=True,
+            path=self.root_path,
+            dockerfile=normpath(join(
+                self.root_path,
+                self.config_path,
+                dockerfile
+            ))
+        )
+
+    pprint(self.__class__.docker_client.images())
