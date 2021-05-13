40f23141fdc9a1986051b1e12b365d823fe47a84
trumanpurnell <truman.purnell@gmail.com>
2021-05-12 21:45:40 -0700

    creates repos to house lambda images

--- awyes/image.py
+++ awyes/image.py
@@ -1,16 +1,35 @@
+from operator import itemgetter
 from re import sub
 from pprint import pprint
 from os.path import normpath, join
 
 
 def deploy_images(self):
-    client = self.__class__.docker_client
+    # Localize the clients
+    ecr = self.__class__.ecr_client
+    docker = self.__class__.docker_client
 
+    # Check if an ecr repository exists, if not create one
+    for repo_name in self.config['repositories']:
+        try:
+            description = ecr.describe_repositories(
+                registryId=self.account_id,
+                repositoryNames=[repo_name]
+            )
+
+            repository = description['repositories'].pop()
+        except:
+            response = ecr.create_repository(repositoryName=repo_name)
+            repository = response['repository']
+
+        self.shared['ecr'][repo_name] = repository
+
+    # Build the images for each lambda
     for dockerfile in self.images:
         image_name = sub(".Dockerfile", "", dockerfile)
-        remote_tag = f"{self.account_id}.dkr.ecr.{self.region}.amazonaws.com/{image_name}"
+        remote_repository = f"{self.account_id}.dkr.ecr.{self.region}.amazonaws.com/{image_name}"
 
-        image, *_ = client.images.build(
+        image, *_ = docker.images.build(
             tag=image_name,
             path=self.root_path,
             dockerfile=normpath(join(
@@ -20,7 +39,5 @@ def deploy_images(self):
             ))
         )
 
-        image.tag(repository=remote_tag)
-
-        for line in client.images.push(repository=remote_tag, stream=True):
-            print(line)
+        image.tag(repository=remote_repository, tag=image_name)
+        print(docker.images.push(repository=remote_repository, tag=image_name))
