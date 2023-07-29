d5d78ad3014c5a99137366662a0bd6eac261ea64
trumanpurnell <truman.purnell@gmail.com>
2023-07-29 14:10:25 -0600

    Remove a few files to simplify versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup, find_packages
+import awyes
+
+from setuptools import setup
 
 setup(
     name='awyes',
     author='Truman Purnell',
     author_email='truman.purnell@gmail.com',
+    version=awyes.__version__,
     description='A package for easy setup and management of resources on AWS',
     url='https://github.com/bb-labs/awyes',
-    packages=find_packages(),
+    packages='awyes',
     install_requires=[
         'boto3',
         'docker',
