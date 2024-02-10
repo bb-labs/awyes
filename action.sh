ca9689967728b1734b65a617bf88566d1f5dd1a3
trumanpurnell <truman.purnell@gmail.com>
2024-02-10 02:05:47 -0700

    No more github workspace?

--- action.sh
+++ action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-awyes --config-path /github/workspace/$1 \
-  --client-path /github/workspace/$2 \
-  --pipfile-path /github/workspace/$3 \
-  --env-path /github/workspace/$4 \
+awyes --config-path $1 \
+  --client-path $2 \
+  --pipfile-path $3 \
+  --env-path $4 \
   $5
