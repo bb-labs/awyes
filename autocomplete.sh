b745273e65eba07b83f8b3edfb5f60615c3d179b
trumanpurnell <truman.purnell@gmail.com>
2024-03-07 01:30:37 -0700

    Autocomplete combs through all dirs now

--- autocomplete.sh
+++ autocomplete.sh
@@ -6,26 +6,12 @@ _arguments \
 '--quiet[only print the actions]' \
 '*:action_name:->action'
 
-function validate_path {
-    if eval realpath $1 >/dev/null 2>/dev/null; then
-        valid_path=$(realpath $1)
-    fi
-    
-    for ((i = 1; i <= $#words - 1; i++)); do
-        if [[ $words[$i] == --config && ! -z $words[$((i + 1))] ]]; then
-            maybe_valid_path=$words[$((i + 1))]
-            if eval realpath $maybe_valid_path >/dev/null 2>/dev/null; then
-                valid_path=$(eval realpath $maybe_valid_path)
-            fi
-        fi
-    done
-    
-    [[ ! -z $valid_path ]]
-}
+
+files=$(find . -name "awyes*.yml" -exec echo -n {} \; -exec echo -n ' ' \;)
+
 
 case $state in
     action)
-        validate_path "./awyes.yml" && _values 'awyes workflows' \
-        $(yq 'select(.) | keys | .[]' $valid_path | grep "[^---]")
+        _values 'awyes actions' $(echo $files | xargs yq 'select(.) | keys | .[]' | grep "[^---]")
     ;;
 esac
