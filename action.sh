4dbfb2552c1d42f35adcbfa4bc436b47217aec2e
trumanpurnell <truman.purnell@gmail.com>
2024-04-14 17:14:23 -0600

    Add flags as passable to awyes gh action

--- action.sh
+++ action.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-awyes --verbose --path $1 $2
+if [ "$3" = true ]; then
+    echo "$3 is true"
+else
+    echo "$3 is not true"
+fi
+
+if [ "$4" = true ]; then
+    echo "$4 is true"
+else
+    echo "$4 is not true"
+fi
+
+awyes --path $1 $2
