14455f88c16310a6a54eb7411f5031d800a48539
trumanpurnell <truman.purnell@gmail.com>
2024-10-20 19:52:52 -0600

    barebones client stubs

---
+++ cloudview/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import clients from "../clients";
+
+function App() {
+  console.log(clients);
+  return <></>;
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const child_process = require("child_process");
 
-const args = process.argv.slice(2);
-console.log(args);
+// Run the cloudview interface
+fs.cpSync(
+  "./awyes.ts",
+  "./node_modules/@the-sage-group/awyes/cloudview/clients/awyes.ts"
+);
+fs.cpSync(
+  "./package.json",
+  "./node_modules/@the-sage-group/awyes/cloudview/clients/package.json"
+);
+child_process.execSync("npm install", {
+  cwd: "./node_modules/@the-sage-group/awyes/cloudview/clients",
+});
+const cloudview = child_process.spawn("npm", [
+  "run",
+  "dev",
+  "--prefix",
+  "./node_modules/@the-sage-group/awyes/cloudview",
+]);
+cloudview.stdout.on("data", (data) => process.stdout.write(data.toString()));
+cloudview.stderr.on("data", (data) => process.stderr.write(data.toString()));
